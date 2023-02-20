a5b19b2cdc3caa14a769f674484ec5bd3db6b18f
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2023-02-20 18:15:20 -0500

    remove soem stuff from start-here

--- subgraph/src/play-dice.ts
+++ subgraph/src/play-dice.ts
@@ -1,23 +1,6 @@
 import { Game as GameEvent } from "../generated/PlayDice/PlayDice";
-import { Game, Player } from "../generated/schema";
+import { Game } from "../generated/schema";
 import { Bytes } from "@graphprotocol/graph-ts";
-
-const updatePlayer = (player: Player, win: boolean): void => {
-  if (win) {
-    player.wins = player.wins + 1;
-  } else {
-    player.losses = player.losses + 1;
-  }
-  player.save();
-};
-
-// eslint-disable-next-line no-undef
-const newPlayer = (id: Bytes): Player => {
-  const player = new Player(id);
-  player.wins = 0;
-  player.losses = 0;
-  return player;
-};
 
 export function handleGame(event: GameEvent): void {
   let game = new Game(event.transaction.hash.concatI32(event.logIndex.toI32()));
@@ -28,18 +11,4 @@ export function handleGame(event: GameEvent): void {
   game.blockTimestamp = event.block.timestamp;
   game.transactionHash = event.transaction.hash;
   game.save();
-
-  const winnerId = event.params.win;
-  let winner = Player.load(winnerId);
-  if (winner == null) {
-    winner = newPlayer(winnerId);
-  }
-  updatePlayer(winner, true);
-
-  const loserId = event.params.loss;
-  let loser = Player.load(loserId);
-  if (loser == null) {
-    loser = newPlayer(loserId);
-  }
-  updatePlayer(loser, false);
 }
